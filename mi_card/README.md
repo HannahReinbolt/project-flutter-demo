d9e71449d2667e2e37ee8c5abc90ed8a44ae52dd
Hannah Reinbolt <hannah@master-logic.com>
2021-07-15 00:08:52 -0500

    created business card app, successful testing

---
+++ mi_card/README.md
@@ -0,0 +1,11 @@
+# i_am_rich
+
+A Flutter app based on a business card. Layout is supposed to be like a professional business card for interested persons.
+This app features custom Google fonts, column and card data structures, icon and color palette usage. 
+
+See the icon and color palette website for help in palette customization: <br />
+https://www.materialpalette.com/
+
+## Completed App Inside
+![Alt text](front.JPG?raw=true "app icon")
+
---
+++ mi_card/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    base()
+  );
+}
+
+class base extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/hannah.png'),
+              ),
+              Text(
+                'Hannah Reinbolt',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'COMPUTER WIZARD',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.indigo.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.indigo.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+1 505 555 5555',
+                    style: TextStyle(
+                      color: Colors.indigo.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    'hannah@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.indigo.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
